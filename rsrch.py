30754f0aef3d520f7686293b85dfa49d5439bf12
Rohan Upadhyay <rohanyupadhyay@gmail.com>
2018-03-08 01:16:59 +0530

    No need for class, use global. Also big fuck up, scroll and click not working.

--- rsrch.py
+++ rsrch.py
@@ -5,66 +5,77 @@ from threading import Timer
 from threading import Thread
 import pynput
 from pynput import mouse
-from pynput.mouse import Button, Controller
 from pynput.mouse import Listener
 from pynput import keyboard
-import math
+from pynput.keyboard import Key, Listener
 import os
 import time
 
 #os.system("tasklist")   
 #print(win32gui.GetDoubleClickTime())
 
+
 mData=[]
     
 lclick=0
 rclick=0
 iloop=1
-def on_click(x, y, button, pressed):
-        if pressed:
-            if button==Button.left:
-                lclick=1
-            if button==Button.right:
-                rclick=1
-        if not pressed:
-            if button==Button.left:
-                lclick=0
-            if button==Button.right:
-                rclick=0
-        mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
-        if iloop==0:
-            return False
 
+def on_click(x, y, button, pressed):
+    print("click")
+    global lclick
+    global rclick
+    global iloop
+
+    if pressed:
+        if button==Button.left:
+            lclick=1
+        if button==Button.right:
+            rclick=1
+    if not pressed:
+        if button==Button.left:
+            lclick=0
+        if button==Button.right:
+            rclick=0
+    #mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+    print(button)
+    if iloop==0:
+        print("False click")
+        return False
 
 
 def on_scroll(x, y, dx, dy):
+    global iloop
     print('Scrolled {0}'.format(
         (x, y)))
     print(dx,dy)
     if iloop==0:
+        print("False click")
         return False
 
 
-def recMousList():        
-    # Collect events until released
-    print("2")
-    with Listener(
-            on_click=on_click,
-            on_scroll=on_scroll) as listener:
-        listener.join()
+
+
 
 
 def recMousPos():
-        while iloop:
-            mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
-            #print(time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None)
+    global iloop
+    while iloop:
+        mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+        #print(time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None)
+        #print (iloop)
+    print("end pos")
+    #threading.current_thread()._stop
+
 
 
 
 def on_press(key):
-    pass
+    print(key)
 
 def on_release(key):
+    global iloop
+    print(key)
     if key == Key.esc:
         # Stop listener
         iloop=0
@@ -73,18 +84,33 @@ def on_release(key):
 
 
 def recKeybPress():
-
-
     # Collect events until released
     with Listener(
             on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()    
+            on_release=on_release,
+            on_click=on_click,
+            on_scroll=on_scroll) as listener:
+        listener.join()
+    print("end press")
+    #threading.current_thread()._stop
+
+t=[]
+
+t.append(threading.Thread(target=recMousPos,args=()))
+
+t.append(threading.Thread(target=recKeybPress,args=()))
+
+t[0].start()
+t[1].start()
+
+#t[0].join()
+#t[1].join()
+
+#print(threading.enumerate())
 
 
-t0=threading.Thread(target=recMousList,args=())
-t1=threading.Thread(target=recMousPos,args=())
-t2=threading.Thread(target=recKeybPress,args=())
-t0.start()
-t1.start()
-t2.start()
+
+
+
+
+print("The End")
--- test.py
+++ test.py
@@ -2,8 +2,19 @@ import win32gui
 import pynput
 from pynput import *
 import time
+from pynput import keyboard
+from pynput.keyboard import Key, Listener
 
 print(pynput.mouse.Controller().position[0])
 print(time.time())
+
+def on_release(key):
+    global iloop
+    print(key.value)
+    if key == Key.esc:
+        # Stop listener
+        iloop=0
+        return False
+
 
 
