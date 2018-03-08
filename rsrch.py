dafbace8e75aa2a2290c97149a93b217cef9e77c
rohanyupadhyay <rohanyupadhyay@gmail.com>
2018-03-08 10:18:31 +0530

    Finally solved all thread related problems. All listeners work fine simultaneously.

--- rsrch.py
+++ rsrch.py
@@ -5,8 +5,8 @@ from threading import Timer
 from threading import Thread
 import pynput
 from pynput import mouse
-from pynput.mouse import Button, Controller
-from pynput.mouse import Listener
+from pynput.mouse import Button
+from pynput.keyboard import Key
 from pynput import keyboard
 import math
 import os
@@ -21,23 +21,28 @@ lclick=0
 rclick=0
 iloop=1
 def on_click(x, y, button, pressed):
-        if pressed:
-            if button==Button.left:
-                lclick=1
-            if button==Button.right:
-                rclick=1
-        if not pressed:
-            if button==Button.left:
-                lclick=0
-            if button==Button.right:
-                rclick=0
-        mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
-        if iloop==0:
-            return False
+    global lclick
+    global rclick
+    global iloop
+    print(button)
+    if pressed:
+        if button==Button.left:
+            lclick=1
+        if button==Button.right:
+            rclick=1
+    if not pressed:
+        if button==Button.left:
+            lclick=0
+        if button==Button.right:
+            rclick=0
+    mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+    if iloop==0:
+        return False
 
 
 
 def on_scroll(x, y, dx, dy):
+    global iloop
     print('Scrolled {0}'.format(
         (x, y)))
     print(dx,dy)
@@ -45,27 +50,32 @@ def on_scroll(x, y, dx, dy):
         return False
 
 
-def recMousList():        
+def recMousList():
+    global mlistener
     # Collect events until released
-    print("2")
-    with Listener(
+    with mouse.Listener(
             on_click=on_click,
-            on_scroll=on_scroll) as listener:
-        listener.join()
+            on_scroll=on_scroll) as mlistener:
+        mlistener.join()
 
 
 def recMousPos():
-        while iloop:
-            mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
-            #print(time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None)
-
+    global iloop
+    while iloop:
+        mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+        #print(time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None)
+    return False
 
 
 def on_press(key):
     pass
 
 def on_release(key):
+    global iloop
+    print(key)
     if key == Key.esc:
+        global mlistener
+        mlistener.stop()
         # Stop listener
         iloop=0
         return False
@@ -73,13 +83,11 @@ def on_release(key):
 
 
 def recKeybPress():
-
-
     # Collect events until released
-    with Listener(
+    with keyboard.Listener(
             on_press=on_press,
-            on_release=on_release) as listener:
-        listener.join()    
+            on_release=on_release) as klistener:
+        klistener.join()    
 
 
 t0=threading.Thread(target=recMousList,args=())
