8a75a8776a92d2afacc8e2871b6c477e82ea26e5
rohanyupadhyay <rohanyupadhyay@gmail.com>
2018-03-12 14:38:01 +0530

    save list to file and read list from file

--- rsrch.py
+++ rsrch.py
@@ -5,31 +5,28 @@ from threading import Timer
 from threading import Thread
 import pynput
 from pynput import mouse
-<<<<<<< HEAD
 from pynput.mouse import Button
 from pynput.keyboard import Key
-=======
-from pynput.mouse import Listener
->>>>>>> 30754f0aef3d520f7686293b85dfa49d5439bf12
 from pynput import keyboard
-from pynput.keyboard import Key, Listener
+import math
 import os
 import time
 
 #os.system("tasklist")   
 #print(win32gui.GetDoubleClickTime())
 
+f=open("data.txt","a")
 
 mData=[]
     
 lclick=0
 rclick=0
 iloop=1
-<<<<<<< HEAD
 def on_click(x, y, button, pressed):
     global lclick
     global rclick
     global iloop
+    global mData
     print(button)
     if pressed:
         if button==Button.left:
@@ -41,46 +38,26 @@ def on_click(x, y, button, pressed):
             lclick=0
         if button==Button.right:
             rclick=0
-    mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+    if len(mData)==0:
+        mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+    else:
+        if mData[-1]!=[time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None]:
+            mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
     if iloop==0:
         return False
-=======
->>>>>>> 30754f0aef3d520f7686293b85dfa49d5439bf12
 
-def on_click(x, y, button, pressed):
-    print("click")
-    global lclick
-    global rclick
-    global iloop
-
-    if pressed:
-        if button==Button.left:
-            lclick=1
-        if button==Button.right:
-            rclick=1
-    if not pressed:
-        if button==Button.left:
-            lclick=0
-        if button==Button.right:
-            rclick=0
-    #mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
-    print(button)
-    if iloop==0:
-        print("False click")
-        return False
 
 
 def on_scroll(x, y, dx, dy):
     global iloop
+    global mData
     print('Scrolled {0}'.format(
         (x, y)))
     print(dx,dy)
     if iloop==0:
-        print("False click")
         return False
 
 
-<<<<<<< HEAD
 def recMousList():
     global mlistener
     # Collect events until released
@@ -88,34 +65,27 @@ def recMousList():
             on_click=on_click,
             on_scroll=on_scroll) as mlistener:
         mlistener.join()
-=======
-
-
->>>>>>> 30754f0aef3d520f7686293b85dfa49d5439bf12
 
 
 def recMousPos():
     global iloop
+    global mData
     while iloop:
-        mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+        if len (mData)==0:
+            mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
+        else:
+            if mData[-1] != [time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None]:  
+                mData.append([time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None])
         #print(time.time(),pynput.mouse.Controller().position[0],pynput.mouse.Controller().position[1],lclick,rclick,None,None)
-<<<<<<< HEAD
     return False
-=======
-        #print (iloop)
-    print("end pos")
-    #threading.current_thread()._stop
-
-
->>>>>>> 30754f0aef3d520f7686293b85dfa49d5439bf12
 
 
 def on_press(key):
-    print(key)
+    print("press:" ,key)
 
 def on_release(key):
     global iloop
-    print(key)
+    print("release:",key)
     if key == Key.esc:
         global mlistener
         mlistener.stop()
@@ -129,35 +99,22 @@ def recKeybPress():
     # Collect events until released
     with keyboard.Listener(
             on_press=on_press,
-<<<<<<< HEAD
             on_release=on_release) as klistener:
         klistener.join()    
-=======
-            on_release=on_release,
-            on_click=on_click,
-            on_scroll=on_scroll) as listener:
-        listener.join()
-    print("end press")
-    #threading.current_thread()._stop
-
-t=[]
-
-t.append(threading.Thread(target=recMousPos,args=()))
-
-t.append(threading.Thread(target=recKeybPress,args=()))
-
-t[0].start()
-t[1].start()
-
-#t[0].join()
-#t[1].join()
-
-#print(threading.enumerate())
 
 
+t0=threading.Thread(target=recMousList,args=())
+t1=threading.Thread(target=recMousPos,args=())
+t2=threading.Thread(target=recKeybPress,args=())
+t0.start()
+t1.start()
+t2.start()
 
 
->>>>>>> 30754f0aef3d520f7686293b85dfa49d5439bf12
+Thread.join(t0)
+Thread.join(t1)
+Thread.join(t2)
+
+f.write(str(mData))
 
 
-print("The End")
--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from pynput import *
 import time
 from pynput import keyboard
 from pynput.keyboard import Key, Listener
+import ast
 
 print(pynput.mouse.Controller().position[0])
 print(time.time())
@@ -16,5 +17,20 @@ def on_release(key):
         iloop=0
         return False
 
+
+a=[]
+a.append([1, 2,4, None])
+a.append([2, 3,5, None])
+a.append([3, 4, 6, None])
+
+if a[-1] == [3, 4, 6, None]:
+    print ("Yes")
+
+f=open("data.txt","r")
+
+#l=ast.literal_eval(f.read())
+#print(l[0])
+
+print(Key.down)
 
 
