9631b4a6fc5f99be5d47ed88f29e4d4be921b348
Rohan Upadhyay <rohanyupadhyay@gmail.com>
2018-05-07 10:14:00 +0530

    added accuracy and cross  entropy

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 import numpy as np
 import ast
-
+#tf.reset_default_graph()
 f1=open('final/pp_in.txt','r')
 f2=open('final/pp_out.txt','r')
 f3=open('final/users.txt','r')
@@ -15,8 +15,8 @@ users=tf.identity(users,name="users")
 
 
 
-learning_rate = 0.0001
-training_steps = 1
+#learning_rate = 0.0001
+training_steps = 10
 display_step = 1
 
 num_input = 19 # number of features
@@ -39,16 +39,17 @@ biases = {
 }
 
 layer = rnn.BasicLSTMCell(num_hidden,forget_bias=1.0)
-cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(num_hidden) for _ in range(2)])
+cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(num_hidden) for _ in range(3)])
 #x=tf.unstack(X,axis=1)
 
 output,state=tf.nn.dynamic_rnn(cell,X,dtype=tf.float32)
 
-prediction=tf.matmul(output[:,-1],weights['out']) +biases['out']
+prediction=tf.nn.softmax(tf.matmul(output[:,-1],weights['out']) +biases['out'])
 prediction=tf.identity(prediction,name="prediction")
-cost=tf.reduce_mean(tf.squared_difference(prediction,Y))
+loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction,labels=Y))
 optimizer=tf.train.AdamOptimizer()
-train=optimizer.minimize(cost)
+train=optimizer.minimize(loss)
+accuracy=tf.reduce_mean((Y*prediction) + (1-Y)*(1-prediction))
 
 saver=tf.train.Saver()
 
@@ -61,18 +62,16 @@ with tf.Session() as sess:
 
 
 
-    i=0
-    while sess.run(cost, feed_dict={X: x,Y:y})>0.2:
+    for i in range(training_steps):
         sess.run(train, feed_dict={X: x,Y:y})
-        i+=1
-        print(i,format(sess.run(cost, feed_dict={X: x,Y:y})))
+        print(i+1,format(sess.run(accuracy, feed_dict={X: x,Y:y})))
         if i%10==0:
             saver.save(sess,"checkpoint/here.ckpt")
             #tf.saved_model.simple_save(sess,"model/",inputs={"X":X},outputs={"prediction":prediction})
     saver.save(sess,"checkpoint/here.ckpt")
-    tf.saved_model.simple_save(sess,"model/",inputs={"X":X},outputs={"prediction":prediction})
+    #tf.saved_model.simple_save(sess,"model/",inputs={"X":X},outputs={"prediction":prediction})
     
     
-    ans=sess.run(output, feed_dict={X: x})
+    #ans=sess.run(output, feed_dict={X: x})
     print(sess.run(prediction, feed_dict={X: x}))
-    
+    print(sess.run(accuracy, feed_dict={X: x,Y:y}))
